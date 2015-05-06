9f3c2fcbfa55ad575cbd873921ade511bb3d753e
ios <jhonnatan@backstagedigital.com.br>
2015-05-06 09:17:23 -0300

    Adição de um ; na linha 222 para não dar erro na minificação

--- jquery-lightbox.js
+++ jquery-lightbox.js
@@ -209,7 +209,7 @@
 				var _minWidth = (plugin_settings.minWidth != "") ? "min-width:"+plugin_settings.minWidth+"px;" : "";
 				_css += '.lightbox-wrapper{ position:fixed; top:0; left:0; width:100%; height:100%; opacity:0; display:none; overflow:scroll;}';
 				_css += '.lightbox-box{ position:relative; top:0; left:0; width:100%; height:100%;'+_minWidth+'}';
-				_css += '.lightbox-background{ position:absolute; top:0; left:0; width:100%; height:100%;'+plugin_settings.background+'}'
+				_css += '.lightbox-background{ position:absolute; top:0; left:0; width:100%; height:100%;'+plugin_settings.background+'}';
 				_css += '.lightbox-content{ position:absolute; top:50%; left:50%;}';
 				
 				if(plugin_settings.close_button != "")
