bee35760a034bed230a6fb111e9b8e3e84b631fd
ios <jhonnatan@backstagedigital.com.br>
2015-05-12 16:46:50 -0300

    Alteração na estrutura do html par auma table

--- jquery-lightbox.js
+++ jquery-lightbox.js
@@ -139,31 +139,22 @@
                 success: function (data)
                 {
                     $(".lightbox-content").css({opacity:0});
-                    $(".lightbox-content").animate({opacity:1},200);
 
-                    //--------------
                     //adiciona o html
-                    if(plugin_settings.dataType == "json")
-                    {
-                        $(".lightbox-content").append(data.html);
-                    }
-                    else
-                    {
-                        $(".lightbox-content").append(data);
-                    }
-
-                    //posiciona o lightbox-content
-                    $('.lightbox-content').css(
-                        {
-                            width:(plugin_settings.width)+"px",
-                            height:(plugin_settings.height)+"px",
-                            marginTop:-(plugin_settings.height/2)+"px",
-                            marginLeft:-(plugin_settings.width/2)+"px"
-                        }
-                    );
+					var _html = (plugin_settings.dataType == "json") ? data.html : data;
+					$(".lightbox-content .lightbox-load-content").append(_html);
+					
+					$('.lightbox-content .lightbox-load-content').css(
+						{
+							width:(plugin_settings.width)+"px",
+							height:(plugin_settings.height)+"px",
+							margin:"0 auto"
+						}
+					);
 
                     //--------------
                     plugin_settings.ajax_sucess.call(this, {data:data});
+					$(".lightbox-content").animate({opacity:1},200);
                 },
                 complete: function (data)
                 {
@@ -186,17 +177,15 @@
         {
 
             //adiciona o html
-            $(".lightbox-content").append(plugin_settings.contentHtml);
-
-            //posiciona o lightbox-content
-            $('.lightbox-content').css(
-                {
-                    width:(plugin_settings.width)+"px",
-                    height:(plugin_settings.height)+"px",
-                    marginTop:-(plugin_settings.height/2)+"px",
-                    marginLeft:-(plugin_settings.width/2)+"px"
-                }
-            );
+            $(".lightbox-content .lightbox-load-content").append(plugin_settings.contentHtml);
+			
+			$('.lightbox-content .lightbox-load-content').css(
+				{
+					width:(plugin_settings.width)+"px",
+					height:(plugin_settings.height)+"px",
+					margin:"0 auto"
+				}
+			);
 
         }
 
@@ -210,11 +199,8 @@
             var _css;
             _css = '<style type="text/css">';
             //-------------
-            var _minWidth = (plugin_settings.minWidth != "") ? "min-width:"+plugin_settings.minWidth+"px;" : "";
             _css += '.lightbox-wrapper{ position:fixed; top:0; left:0; width:100%; height:100%; opacity:0; display:none; overflow:scroll; z-index:'+plugin_settings.zIndex+';}';
-            _css += '.lightbox-box{ position:relative; top:0; left:0; width:100%; height:100%;'+_minWidth+'}';
-            _css += '.lightbox-background{ position:absolute; top:0; left:0; width:100%; height:100%;'+plugin_settings.background+';}';
-            _css += '.lightbox-content{ position:absolute; top:50%; left:50%;}';
+             _css += '.lightbox-background{ position:absolute; top:0; left:0; width:100%; height:100%;'+plugin_settings.background+';}';
 
             if(plugin_settings.close_button != "")
             {
@@ -236,27 +222,28 @@
         function getHtml (){
 
             var _html;
-
-            _html  = '<div class="lightbox-wrapper">';
-            _html += '<div class="lightbox-box">';
-            //---------------------
-            _html += '<div class="lightbox-background"></div>';
-
-            if(plugin_settings.show_loader != "")
-            {
-                _html += '<div class="lightbox-loader"></div>';
-            }
-
-            _html += '<div class="lightbox-content">';
-            //só cria o botão de fechar do lightbox caso tenha a propriedade \close_button\ na instanciação
-            if(plugin_settings.close_button != "")
-            {
-                _html += '<a href="#" class="lightbox-button-close"></a>';
-            }
-            _html += '</div>';
-            //---------------------
-            _html  += '</div>';
-            _html  += '</div>';
+			
+			_html = '<div class="lightbox-wrapper">';
+				_html += '<table style=" width:100%; height:100%; position:relative;">';
+					
+					if(plugin_settings.show_loader != "")
+					{
+						_html += '<div class="lightbox-loader"></div>';
+					}
+					_html += '<tr>';
+						_html += '<td class="lightbox-content" style=" padding:30px;">';
+						_html += '<div class="lightbox-background"></div>';
+							_html += '<div style=" position:relative;" class="lightbox-load-content">';
+								//só cria o botão de fechar do lightbox caso tenha a propriedade \close_button\ na instanciação
+								if(plugin_settings.close_button != "")
+								{
+									_html += '<a href="#" class="lightbox-button-close"></a>';
+								}
+							_html += '</div>';
+						_html += '</td>';
+					_html += '</tr>';
+				_html += '</table>';
+			_html += '</div>';
 
             return _html;
 
